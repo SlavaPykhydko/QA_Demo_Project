6334eb2c3ebe34770071b39fe6f09f2817532ecb
SlavaPykhydko <slava17puh92@gmail.com>
2026-03-06 19:38:59 +0200

    Added positive tests test_scheme_orders_list, test_quantity_items_for_different_status, test_quantity_items_for_pagination

--- tests/api/online_orders_test.py
+++ tests/api/online_orders_test.py
@@ -1,6 +1,8 @@
 import pytest
 import pytest_check as check
 
+from src.models.online_orders_list import OrdersResponse
+
 # We need to get this data from db,
 # but we don't have this opportunity now, so we just use data from response
 count_all_orders_from_db = 21
@@ -77,8 +79,57 @@ def test_sum_done_and_cancel_orders(online_orders_api):
     total_count_cancel_orders = online_orders_api._get_json_value(response_cancel_orders, "totalCount")
 
     check.equal(count_all_orders_from_db,
-                count_done_orders_from_db + count_cancel_orders_from_db, "Some of the count from db is wrong")
+                count_done_orders_from_db + count_cancel_orders_from_db,
+                "Some of the count from db is wrong")
     check.equal(total_count_all_orders,
-                total_count_done_orders + total_count_cancel_orders, "Some of the count from response is wrong")
+                total_count_done_orders + total_count_cancel_orders,
+                "Some of the count from response is wrong")
+
+test_data_3 = [
+    ({"status": "All"}),
+    ({"status": "Done"}),
+    ({"status": "Cancel"})
+]
+
+# Generating good-looking names for reports
+test_ids_3 = [f"limit=40_page=0_status_{d3['status']}" for d3 in test_data_3]
+
+@pytest.mark.parametrize("inputs", test_data_3, ids=test_ids_3)
+def test_scheme_orders_list(online_orders_api, inputs):
+    response = online_orders_api.get_online_orders(page=0, limit=40, status=inputs["status"])
+    OrdersResponse(**response.json())
+
+test_data_2 = [
+    ({"page": 0, "limit": 40, "status": "All"}),
+    # ({"page": 0, "limit": 10, "status": "All"}),
+    # ({"page": 1, "limit": 10, "status": "All"}),
+    # ({"page": 2, "limit": 10, "status": "All"}),
+    ({"page": 0, "limit": 40, "status": "Done"}),
+    ({"page": 0, "limit": 40, "status": "Cancel"})
+]
+
+# Generating good-looking names for reports
+test_ids_2 = [f"limit_{d2['limit']}_page_{d2['page']}_status_{d2['status']}" for d2 in test_data_2]
+
+@pytest.mark.parametrize("inputs", test_data_2, ids=test_ids_2)
+def test_quantity_items_for_different_status(online_orders_api, inputs):
+    response = online_orders_api.get_online_orders(
+        page=inputs["page"],
+        limit=inputs["limit"],
+        status=inputs["status"]
+    )
+    parsed_data = OrdersResponse(**response.json())
+
+    check.equal(len(parsed_data.items), parsed_data.totalCount,
+                "The number of items is not equal totalCount from response")
 
 
+def test_quantity_items_for_pagination(online_orders_api):
+    all_items = []
+    for page in [0,1,2]:
+        response = online_orders_api.get_online_orders(page=page, limit=10, status="All")
+        parsed_data = OrdersResponse(**response.json())
+        all_items.extend(parsed_data.items)
+
+    check.equal(len(all_items), count_all_orders_from_db,
+                "The number of items is not equal the quantity from db")
