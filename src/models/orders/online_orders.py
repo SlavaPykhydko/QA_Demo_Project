9c159b87ad066806434639cb84aa8afbe16b68ca
SlavaPykhydko <slava17puh92@gmail.com>
2026-03-10 17:58:28 +0200

    Added to model annotations

--- src/models/orders/online_orders.py
+++ src/models/orders/online_orders.py
@@ -1,21 +1,21 @@
-from pydantic import BaseModel, ConfigDict
-from typing import List
+from pydantic import BaseModel, ConfigDict, Field, StringConstraints
+from typing import List, Annotated
 
 class OrderItem(BaseModel):
     #Allow to ignore fields which aren't described
     model_config = ConfigDict(extra='ignore')
     # Describing fields of one order
     id: int
-    name: str
+    name: Annotated[str, StringConstraints(strip_whitespace=True, min_length=1)]
     price: float
     totalBonus: float
-    createdOn: str
-    orderStatus: str
-    status: str
-    statusGroup: str
-    seller: str
+    createdOn: Annotated[str, StringConstraints(strip_whitespace=True, min_length=1)]
+    orderStatus: str = Field(min_length=1)
+    status: str = Field(min_length=1)
+    statusGroup: str = Field(min_length=1)
+    seller: Annotated[str, StringConstraints(strip_whitespace=True, min_length=1)]
     quantity: int
-    type: str
+    type: str = Field(min_length=1)
     deliveryId: int
     goods: List[str]
 
